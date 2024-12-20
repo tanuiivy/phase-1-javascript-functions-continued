d0e11606389865b95cffcaebd27bc4b9f36d18b8
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-12-20 19:23:25 +0300

    Functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// Define the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Define the mondayWork function
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+// Define the wrapAdjective function
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+// Exporting functions if required for testing
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+};
